6f827d375065e8272021dbcb2de77463bb1e0656
Rozzaq <abdurozzaq.nurul@mail.sirclo.com>
2021-08-17 09:09:44 +0700

    Initial

---
+++ src/Aritmatika.php
@@ -0,0 +1,16 @@
+<?php 
+
+namespace Abdurozzaq\Aritmatika;
+
+class Aritmatika
+{
+    public function greet($greet = "Hello World")
+    {
+        return $greet;
+    }
+
+    public function sum($number1, $number2)
+    {
+        return $number1 + $number2;
+    }    
+}
